8c51d299f2ff25f41cd7e527e6622ff8e635b238
Larissa <lara.adler@hotmail.com>
2023-06-13 10:35:43 -0300

    Atualizações de espaços

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,20 +2,13 @@ import { Container } from './styles';
 
 import { NavBar } from '../../components/NavBar';
 
-import { ButtonSign } from '../../components/ButtonSign';
 
-import { HeaderSign } from '../../components/HeaderSign';
 
 export function Home() {
   return (
     <Container>
-      <main>
-        
-      </main>
+  
        <NavBar />
-       <ButtonSign title="Entrar"/>
-       <HeaderSign />
-      
       
     </Container>
   )
